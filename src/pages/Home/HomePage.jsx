5111e8e7b6e15bca1217e92a8fd9f8a31dbe4949
Priya Chauhan <Priya@123>
2024-06-26 21:37:03 +0530

    created project

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -10,11 +10,11 @@ const HomePage = () => {
     }
   return (
       <div className='flex items-center justify-center h-screen'>
-       <div className='w-full max-w-xs  flex flex-col gap-5  items-center p-4 border border-emerald-500 rounded'>
+       <div className='w-full max-w-xs  flex flex-col gap-5  items-center p-4 border-2 border-sky-500 rounded'>
         <div>
             <p className='font-bold text-2xl p-4 text-sky-600' >Join the meeting</p>
         </div>
-        <input type="text" className='border rounded-xl px-4 py-2 w-full' value={value} onChange={(e)=> setValue(e.target.value)} placeholder='Enter Room Code' />
+        <input type="text" className='border border-sky-400 rounded-xl px-4 py-2 w-full' value={value} onChange={(e)=> setValue(e.target.value)} placeholder='Enter Room Code' />
         <button onClick={handleJoinRoom} className='bg-sky-600 text-white w-full max-w-xs mx-auto p-4 border border-emerald-500 rounded-xl'>Join</button>
     </div>
    </div>
